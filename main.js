bdb0ae86aebbdaf782f192d308f2e8166ba172b6
Alan <holasoyalan9@gmail.com>
2022-09-21 22:14:58 -0500

    Proyecto terminadoooo

--- main.js
+++ main.js
@@ -15,8 +15,10 @@ periodsContainer.addEventListener('click',(event) =>{
       pushElements('monthly');
       // console.log('Monthly!!!!');
     }else if(event.target.outerText === 'Daily'){
+      pushElements('daily')
       console.log('Daily!!');
     }else if(event.target.outerText === 'Weekly'){
+      pushElements('weekly')
       console.log('Weekly!!!');
     }
     // console.log(event.target.outerText)
@@ -44,9 +46,19 @@ const pushElements = async (period) => {
     return item.timeframes
   });
   
-  const timeFramesPrevious =  timeFrames.flatMap(item => item.monthly).flatMap(item => item.previous);
-  const timeFramesCurrent =  timeFrames.flatMap(item => item. monthly).flatMap(item => item.current);
-  // console.log(timeFramesCurrent, timeFramesPrevious);
+  console.log(timeFrames);
+  // Time Frames Monthly
+  const timeFramesPreviousMonthly =  timeFrames.flatMap(item => item.monthly).flatMap(item => item.previous);
+  const timeFramesCurrentMonthly =  timeFrames.flatMap(item => item.monthly).flatMap(item => item.current);
+  // Time Frames Monthly
+  const timeFramesPreviousWeekly =  timeFrames.flatMap(item => item.weekly).flatMap(item => item.previous);
+  const timeFramesCurrentWeekly =  timeFrames.flatMap(item => item.weekly).flatMap(item => item.current);
+  // Time Frames Monthly
+  const timeFramesPreviousDaily =  timeFrames.flatMap(item => item.daily).flatMap(item => item.previous);
+  const timeFramesCurrentDaily =  timeFrames.flatMap(item => item.daily).flatMap(item => item.current);
+  
+
+  console.log(timeFramesCurrentDaily, timeFramesPreviousDaily);
 
   cardsWithoutPerson.map((item, counter=0) => {
     // const hourItemsDaily = timeFrames[counter].daily;
@@ -74,8 +86,23 @@ const pushElements = async (period) => {
     const hoursLabel = document.createElement('p');
     const weekHoursLabel = document.createElement('p');
     // const hours = document.createTextNode(`si`);
-    const hours = document.createTextNode(`${timeFramesCurrent[counter]}hrs`);
-    const hourPerWeek = document.createTextNode(`Last Week - ${timeFramesPrevious[counter]}hrs`);
+    let hours = '';
+    let hourPerWeek = '';
+    if(period === 'monthly'){
+      hours = document.createTextNode(`${timeFramesCurrentMonthly[counter]}hrs`);
+      hourPerWeek = document.createTextNode(`Last Week - ${timeFramesPreviousMonthly[counter]}hrs`);
+      console.log(period);
+    }else if(period === 'daily'){
+      hours = document.createTextNode(`${timeFramesCurrentDaily[counter]}hrs`);
+      hourPerWeek = document.createTextNode(`Last Week - ${timeFramesPreviousDaily[counter]}hrs`);
+      console.log(period);
+    }else if(period === 'weekly'){
+      hours = document.createTextNode(`${timeFramesCurrentWeekly[counter]}hrs`);
+      hourPerWeek = document.createTextNode(`Last Week - ${timeFramesPreviousWeekly[counter]}hrs`);
+      console.log(period);
+    };
+
+    
     // const hours = document.createTextNode(`10`);
     hoursLabel.appendChild(hours);
     weekHoursLabel.appendChild(hourPerWeek);
